2b58e78bca01d2a601fdbf433b4c96389020d65a
Kevin Sawade <kevin.sawade@uni-konstanz.de>
2020-02-28 14:20:52 +0100

    Corrected mistakes

---
+++ helloworld.pyx
@@ -0,0 +1,15 @@
+def hello_world():
+    """This is the docstring which gets transferred into the .so
+    
+    Additionally to printing hello world, I will also do some calculations.
+    
+    Args:
+        No args lol.
+        
+    Returns:
+        None
+        
+    """
+    for i in range(9):
+        y = i ** i ** i
+    print("Hello World")
